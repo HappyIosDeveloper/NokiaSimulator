6ef4e9b4c9eab2cd4dded7bf696ed24bdb1f80fc
Alfredo <lovlyboy@gmail.com>
2020-06-18 20:18:17 +0430

    Initial Commit

---
+++ nokia show off/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nokia show off
+//
+//  Created by Alfredo Uzumaki on 6/18/20.
+//  Copyright © 2020 Alfredo Uzumaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
