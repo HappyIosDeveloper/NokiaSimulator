1e2ea242dcaf078ca4472143d749470a079ca60c
Alfredo <lovlyboy@gmail.com>
2020-06-19 22:02:34 +0430

    Added dial pad sounds.

--- nokia show off/ViewController.swift
+++ nokia show off/ViewController.swift
@@ -7,18 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var topLogoImageView: UIImageView!
     @IBOutlet weak var screenImageView: UIImageView!
+    @IBAction func DialPadButtonAction(_ sender: Any) {
+        if let butt = sender as? UIButton {
+            dialPadButtonAction(butt)
+        }
+    }
+    @IBAction func leftFunctionButtonAction(_ sender: Any) {
+    }
+    @IBAction func rightFunctionButtonAction(_ sender: Any) {
+    }
+    @IBAction func callButtonAction(_ sender: Any) {
+    }
+    @IBAction func endCallButtonAction(_ sender: Any) {
+    }
+    
+    var player:AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupPage()
     }
     
-
     override var prefersStatusBarHidden: Bool {
         return true
     }
@@ -28,3 +43,27 @@ class ViewController: UIViewController {
     }
 }
 
+
+// MARK: - Action Function
+extension ViewController {
+    
+    func dialPadButtonAction(_ button:UIButton) {
+        switch button.tag {
+        case 1...9: play(button.tag.description)
+        case 11: play("0")
+        default: play((0...9).randomElement()!.description)
+        }
+    }
+    
+    func play(_ fileName: String) {
+        if let url = Bundle.main.url(forResource: fileName, withExtension: "mp3") {
+            do {
+                player = try AVAudioPlayer(contentsOf: url)
+                player.prepareToPlay()
+                player.play()
+            } catch let error as NSError {
+                print(error.description)
+            }
+        }
+    }
+}
